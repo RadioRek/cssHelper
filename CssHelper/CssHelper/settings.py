5b1871ff70cebc97b495320dcad24ffd27a31c29
RadioRek <nic.ortega@gmail.com>
2025-12-08 01:57:33 +0000

    equisde desde ssh

--- CssHelper/CssHelper/settings.py
+++ CssHelper/CssHelper/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-9i&j)#qa94uoyb037v(w$t#3vze1h5mik6&5m(@)b3a7egaya(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['novaspace.cl', 'www.novaspace.cl']
+
+ALLOWED_HOSTS = ['novaspace.cl', 'www.novaspace.cl', 'localhost', '127.0.0.1']
 
 # Application definition
 
---
+++ CssHelper/staticfiles/js/port.js
@@ -0,0 +1,170 @@
+let lenis = new Lenis({
+    smooth: true,
+    lerp: 0.08,
+    wheelMultiplier: 0.3
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+
+let durations = {
+    "#sobreMi": 1,
+    "#experiencia": 1,
+    "#portfolio": 2,
+    "#contacto": 2
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault()
+        const id = link.getAttribute('href')
+        const target = document.querySelector(id)
+        lenis.scrollTo(target, {
+            duration: durations[id]
+        })
+    })
+})
+
+const cube = document.querySelector('.cube');
+
+let isDragging = false;
+let startX = 0;
+let startY = 0;
+let currentRotX = -20;
+let currentRotY = 20;
+let targetRotX = currentRotX;
+let targetRotY = currentRotY;
+
+cube.style.transform = `rotateX(${currentRotX}deg) rotateY(${currentRotY}deg)`;
+
+cube.addEventListener('mousedown', e => {
+    isDragging = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+document.addEventListener('mousemove', e => {
+    if (!isDragging) return;
+
+    const dx = e.clientX - startX;
+    const dy = e.clientY - startY;
+
+    targetRotY = currentRotY + dx * 0.25;
+    targetRotX = currentRotX - dy * 0.25;
+});
+
+function animate() {
+    currentRotX += (targetRotX - currentRotX) * 0.15;
+    currentRotY += (targetRotY - currentRotY) * 0.15;
+
+    cube.style.transform = `rotateX(${currentRotX}deg) rotateY(${currentRotY}deg)`;
+    requestAnimationFrame(animate);
+}
+
+
+function encender(el) {
+    let face = el.parentElement;
+    let textoFace = face.querySelector('.head6');
+
+    if (el.classList.contains("candleSvgOn")) {
+
+        el.classList.remove("candleSvgOn");
+        el.classList.add("candleSvgOff");
+        el.classList.remove("animCandleFlicker");
+
+        textoFace.textContent = "Click the candle!";
+        textoFace.classList.remove("animTextoParpadear");
+
+        face.classList.remove("animBordeParpadear");
+        face.style.borderRadius = "0rem";
+
+        if (face.classList.contains("front")) {
+            face.style.transform = "translateZ(75px)";
+        }
+
+        if (face.classList.contains("back")) {
+            face.style.transform = "rotateY(180deg) translateZ(75px)";
+        }
+
+        if (face.classList.contains("left")) {
+            face.style.transform = "rotateY(-90deg) translateZ(75px)";
+        }
+
+        if (face.classList.contains("right")) {
+            face.style.transform = "rotateY(90deg) translateZ(75px)";
+        }
+
+        if (face.classList.contains("top")) {
+            face.style.transform = "rotateX(90deg) translateZ(75px)";
+        }
+
+        if (face.classList.contains("bottom")) {
+            face.style.transform = "rotateX(-90deg) translateZ(75px)";
+        }
+
+
+    } else {
+
+        el.classList.remove("candleSvgOff");
+        el.classList.add("candleSvgOn");
+        el.classList.add("animCandleFlicker");
+
+        textoFace.textContent = "The candle is lit!";
+        textoFace.classList.add("animTextoParpadear");
+
+        face.classList.add("animBordeParpadear");
+        face.style.borderRadius = "1rem";
+
+        if (face.classList.contains("front")) {
+            face.style.transform = "translateZ(90px)";
+        }
+
+        if (face.classList.contains("back")) {
+            face.style.transform = "rotateY(180deg) translateZ(90px)";
+        }
+
+        if (face.classList.contains("left")) {
+            face.style.transform = "rotateY(-90deg) translateZ(90px)";
+        }
+
+        if (face.classList.contains("right")) {
+            face.style.transform = "rotateY(90deg) translateZ(90px)";
+        }
+
+        if (face.classList.contains("top")) {
+            face.style.transform = "rotateX(90deg) translateZ(90px)";
+        }
+
+        if (face.classList.contains("bottom")) {
+            face.style.transform = "rotateX(-90deg) translateZ(90px)";
+        }
+    }
+
+}
+
+
+animate();
+
+
+let inputs = document.querySelectorAll('.inputCustom, .textAreaCustom');
+
+inputs.forEach(input => {
+    input.addEventListener('focus', () => {
+        input.classList.remove('animBordeTextoParpadear');
+        input.classList.add('animBordeTextoParpadearNaranja');
+    });
+
+    input.addEventListener('blur', () => {
+        input.classList.remove('animBordeTextoParpadearNaranja');
+        input.classList.add('animBordeTextoParpadear');
+    });
+
+});
